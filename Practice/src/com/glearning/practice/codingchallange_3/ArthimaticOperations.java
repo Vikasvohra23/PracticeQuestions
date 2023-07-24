7e987209a21c4102292b9bfc0e3e048fb7b05915
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-24 17:38:06 +0530

    practice question 3 is been commited

---
+++ Practice/src/com/glearning/practice/codingchallange_3/ArthimaticOperations.java
@@ -0,0 +1,22 @@
+package com.glearning.practice.codingchallange_3;
+
+public class ArthimaticOperations {
+	public static void main(String[] args) {
+		float num1 = 100;
+		float num2 = 20;
+		System.out.println("Number 1 is "+ num1 + " Number 2 is " + num2 );
+		
+		float add = num1+num2;
+		float sub = num1-num2;
+		float mult = num1*num2;
+		float div = num1/num2;
+		
+		System.out.println("Addition of these two numbers is " + add);
+		System.out.println("Subtaraction of these two numbers is " + sub);
+		System.out.println("Multiplication of these two numbers is " + mult);
+		System.out.println("Division of these two numbers is " + div);
+		
+		
+	}
+
+}
