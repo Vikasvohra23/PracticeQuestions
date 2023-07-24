9dedd4a375d61026b623f93acff5733db3ed1d7f
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-24 23:01:37 +0530

    solved

---
+++ Practice/src/com/glearning/practice/codingchallange_4/EvenNumber.java
@@ -0,0 +1,12 @@
+package com.glearning.practice.codingchallange_4;
+
+class Main {public static void main(String[] args) {
+	int no = 55;
+	if (no % 2 == 0)
+		System.out.println("Number is Even ");
+	else
+		System.out.println("Number is  odd");
+	}
+}
+
+
