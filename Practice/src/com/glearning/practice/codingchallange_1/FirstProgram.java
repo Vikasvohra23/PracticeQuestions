76808f103eb6d6d53c9dac46f5932beaf25d24a6
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-24 16:28:53 +0530

    First Commit

---
+++ Practice/src/com/glearning/practice/codingchallange_1/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.glearning.practice.codingchallange_1;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("My First Program in Java");	
+	}
+}
+
+
---
+++ Practice/src/com/glearning/practice/codingchallange_2/Variables.java
@@ -0,0 +1,17 @@
+package com.glearning.practice.codingchallange_2;
+
+public class Variables {
+	public static void main(String[] args) {
+		int rollno = 4;
+		float height = 172;
+		char gender ='M';
+		boolean illness = false;
+		String name = "Vikas";
+		double percentage = 99.9;
+		
+		System.out.println("Name of the Roll No. " + rollno + " is " + name + " he is a " + gender + " His Height is " + height + " he scored "+ percentage + " percent in his exams");
+		
+		
+	}
+
+}
