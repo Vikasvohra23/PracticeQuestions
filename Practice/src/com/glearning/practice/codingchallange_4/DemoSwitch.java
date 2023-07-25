071d65127855562d660d274166c87070dc604cd1
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-25 17:30:02 +0530

    Demo Switch question solved

---
+++ Practice/src/com/glearning/practice/codingchallange_4/DemoSwitch.java
@@ -0,0 +1,19 @@
+package com.glearning.practice.codingchallange_4;
+
+public class DemoSwitch {
+	public static void Main(String[] args) {
+		int Num1 = 2;
+		switch(Num1) {
+		case 1: System.out.println("Number is Neither Prime Not Composite");
+		break;
+		case 2: System.out.println("Number is a Prime Number");
+		break;
+		case 4: System.out.println("Number is a Composite Number");
+		break;
+		
+		}
+	}
+
+}
+
+
