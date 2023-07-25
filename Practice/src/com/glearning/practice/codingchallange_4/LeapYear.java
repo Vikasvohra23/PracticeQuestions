9af187788f7ec7d2ca4c007631bbe34f441c4e85
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-25 17:03:25 +0530

    Soved

--- Practice/src/com/glearning/practice/codingchallange_4/LeapYear.java
+++ Practice/src/com/glearning/practice/codingchallange_4/LeapYear.java
@@ -9,14 +9,14 @@ public class LeapYear {
 		}
 		
 		else if (year % 100 == 0) {
-			System.out.println(" not a leap year");
+			System.out.println(" Not a Leap Year");
 			}
 		
 		else if (year % 4 == 0) {
-			System.out.println("leap year");
+			System.out.println("Leap Year");
 			}
 		else {
-			System.out.println("not a leap year");
+			System.out.println("Not a Leap Year");
 		}
 		
 
---
+++ Practice/src/com/glearning/practice/codingchallange_4/SwapNumbers.java
@@ -0,0 +1,17 @@
+package com.glearning.practice.codingchallange_4;
+
+public class SwapNumbers {
+	public static void main(String[] args) {
+		int Num1, Num2;
+		Num1 = 9;
+		Num2 = 7;
+		System.out.println("Number 1 is " + Num1 + " & Number 2 is " + Num2);
+		Num1 = Num1 + Num2;
+		Num2 = Num1 - Num2;
+		Num1 = Num1 - Num2;
+		System.out.println("Number 1 is now " + Num1 + " & Number 2 is now " + Num2);
+				
+		
+	}
+
+}
