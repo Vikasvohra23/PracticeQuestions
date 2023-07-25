7f8994931afd749110383e12e3a504decad9db02
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-25 17:35:07 +0530

    Switch Practice

--- Practice/src/com/glearning/practice/codingchallange_4/DemoSwitch.java
+++ Practice/src/com/glearning/practice/codingchallange_4/DemoSwitch.java
@@ -1,8 +1,8 @@
 package com.glearning.practice.codingchallange_4;
 
 public class DemoSwitch {
-	public static void Main(String[] args) {
-		int Num1 = 2;
+	public static void main(String[]args) {
+		int Num1 = 4;
 		switch(Num1) {
 		case 1: System.out.println("Number is Neither Prime Not Composite");
 		break;
