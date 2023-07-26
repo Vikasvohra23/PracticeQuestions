9031a91b0ef4cc4111ae85b3051d3ea05ae3cd04
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-07-26 16:42:03 +0530

    solved

---
+++ Practice/src/com/glearning/practice/codingchallange_5/TableTwo.java
@@ -0,0 +1,18 @@
+package com.glearning.practice.codingchallange_5;
+
+public class TableTwo {
+	public static void main(String[] args) {
+		int i = 1;
+		for(; ; i++) {
+			if(i%2!=0) 
+				continue;
+			
+			else if(i>20)
+			break;
+			
+			else
+			System.out.println(i);
+		}
+	}
+
+}
