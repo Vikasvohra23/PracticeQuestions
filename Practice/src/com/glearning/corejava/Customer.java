facd856158af3ac63277ccb9379ab06fee34a44c
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-08-03 16:25:04 +0530

    Week 1 coding challenge 3 is solved

---
+++ Practice/src/com/glearning/corejava/Customer.java
@@ -0,0 +1,10 @@
+package com.glearning.corejava;
+
+public class Customer {
+	private int age = 22;
+	protected String name = "Vikas Vohra";
+	public void info() {
+		System.out.println("The Age of this customer is " + age + " years");
+	}
+
+}
---
+++ Practice/src/com/glearning/corejava/CustomerDriver.java
@@ -0,0 +1,11 @@
+package com.glearning.corejava;
+
+public class CustomerDriver {
+	public static void main(String[] args) {
+		Customer user = new Customer();
+		System.out.println("The Name of The Customer is " + user.name);
+		user.info();
+		
+	}
+
+}
