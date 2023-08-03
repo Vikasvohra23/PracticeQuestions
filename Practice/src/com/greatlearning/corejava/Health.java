0d54621d8d713a40cce5c7f0b7a177df663b7715
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-08-03 16:12:09 +0530

    Week 1 Coding challenge 2 is solved

---
+++ Practice/src/com/greatlearning/corejava/Health.java
@@ -0,0 +1,9 @@
+package com.greatlearning.corejava;
+
+public class Health {
+	protected int age = 22;
+	protected float weight = 60;
+	protected float bmi =  19;
+}
+
+
---
+++ Practice/src/com/greatlearning/corejava/HealthDriver.java
@@ -0,0 +1,12 @@
+package com.greatlearning.corejava;
+
+public class HealthDriver {
+	public static void main(String[] args) {
+		Health user = new Health();
+		System.out.println("The Age of the user is " + user.age);
+		System.out.println("The Weight of the user is " + user.weight);
+		System.out.println("The BMI of the user is " + user.bmi);
+		
+		}
+
+}
