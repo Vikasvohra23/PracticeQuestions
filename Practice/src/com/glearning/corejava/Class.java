86d197bc58aba68efe5975ec83e63491c6e31751
Vikasvohra23 <127021732+Vikasvohra23@users.noreply.github.com>
2023-08-24 16:23:06 +0530

    coding challenge 9 week 2 completed

---
+++ Practice/src/com/glearning/corejava/Class.java
@@ -0,0 +1,13 @@
+package com.glearning.corejava;
+
+public class Class {
+	
+	void copyMe() {
+			System.out.println("Inside copy me method");
+		}
+	
+	public static void main(String[] args) {
+		Class obj = new Class();
+		obj.copyMe();
+		}
+}
